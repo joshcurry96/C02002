346da6850e31d3f2d1f3dc30f413e7d98f15690d
josh curry <josh curry>
2017-02-22 19:08:53 +0000

    dfsadf

--- src/financialCalculator.java
+++ src/financialCalculator.java
@@ -1,44 +1,100 @@
+import com.sun.corba.se.spi.orbutil.fsm.Guard.Result;
+
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.geometry.Pos;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.control.TextFormatter;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+import javafx.util.converter.NumberStringConverter;
 
 public class financialCalculator extends Application{
 
+	public double presentValue;
+	public double futureValue;
 	
-	public void presentvalue() {
+	public double futureValue(TextField iT, TextField iR, TextField tP) {
+		double investment = Double.parseDouble(iT.getText()); 
+		double interestRate = Double.parseDouble(iR.getText());
+		double timePeriod = Double.parseDouble(tP.getText());
 		
+		double power = Math.pow((1+interestRate), timePeriod);
+		
+		double result = investment * power;
+		System.out.println(result);
+		return result;
+	}
+	
+	public double presentValue(TextField fv2, TextField ir2, TextField tp2){
+		double futureValue = Double.parseDouble(fv2.getText()); 
+		double interestRate = Double.parseDouble(ir2.getText());
+		double timePeriod = Double.parseDouble(tp2.getText());
+		
+		double power = Math.pow((1+interestRate), timePeriod);
+		
+		double result = futureValue/power;
+		System.out.println(result);
+		return result;
 	}
 	
 	
-	
-	
 	public Parent content(){
+		
 		VBox input = new VBox(20);
+		
 		input.setAlignment(Pos.CENTER);
-		Label label1 = new Label("Future and Present values");
-		TextField number1 = new TextField("Enter number 1:");
-		number1.getText();
-		TextField number2 = new TextField("Enter number 2:");
-		number2.getText();
-		Label label2 = new Label("Please choose the calculation you wish to perform");
-		input.getChildren().addAll(label1,number1, number2, label2);
+	
+		Label label1 = new Label("Future value");
+		
+		TextField initialInvestment = new TextField();
+		
+		TextField interestRate = new TextField();
+		
+		TextField timePeriod = new TextField();
+	
+		Label label2 = new Label("Press the button to perform the calculation");
+		
+		input.getChildren().addAll(label1, initialInvestment, interestRate, timePeriod, label2);
 		
 		HBox buttons = new HBox(10);
 		buttons.setAlignment(Pos.CENTER);
 		
 		Button presentValue = new Button("Present Value");
 		
-		Button futureValue = new Button("Future Value");
 		
-		buttons.getChildren().addAll(presentValue, futureValue);
+		Button futureValue = new Button("Future Value");
+		futureValue.setOnAction(event -> futureValue(initialInvestment, interestRate, timePeriod));
+		TextField futureValueResult = new TextField();
+		buttons.getChildren().addAll(futureValue, futureValueResult);
+		
+		VBox input2 = new VBox(20);
+		input2.setAlignment(Pos.CENTER);
+		Label label3 = new Label("Present Value");
+		TextField futureValue2 = new TextField("Please enter the Future Value: ");
+		
+		TextField interestRate2 = new TextField("Please enter the interest rate in %");
+		
+		TextField timePeriod2 = new TextField("Please enter the time period");
+		
+		presentValue.setOnAction(event -> presentValue(futureValue2, interestRate2, timePeriod2));
+		
+		Label label4 = new Label("Press the button to perform the calculation");
+		
+		input2.getChildren().addAll(label3, futureValue2, interestRate2, timePeriod2, label4);
+		
+		HBox buttons2 = new HBox(10);
+		buttons2.setAlignment(Pos.CENTER);
+		TextField presentValueResult = new TextField();
+		buttons2.getChildren().addAll(presentValue, presentValueResult);
+		
 		
 		Pane paneBack = new Pane();
 		Button back = new Button("Back");
@@ -47,7 +103,7 @@ public class financialCalculator extends Application{
 
 		VBox tab1 = new VBox(10);
 		tab1.setPrefSize(500, 250);
-		tab1.getChildren().addAll(input, buttons, paneBack);
+		tab1.getChildren().addAll(input, buttons,input2,buttons2, paneBack);
 		return tab1;
 		
 				
@@ -107,7 +163,7 @@ public class financialCalculator extends Application{
 		Label label = new Label("Please choose which calculations you would like to perform");
 		
 		Pane pane = new Pane();
-		pane.setPrefSize(500,250);
+		pane.setPrefSize(1000,800);
 		pane.getChildren().addAll(label, tab1, tab2);
 		tab1.setLayoutX(50);
 		tab1.setLayoutY(100);
